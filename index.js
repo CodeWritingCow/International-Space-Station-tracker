cc3d9bf00480c3c609d24fdd000217f85537167d
Gary Pang <gary.pang@gmail.com>
2017-08-16 02:15:02 -0400

    Get station space coordinates every 3 seconds

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var got = require('got');
 
+var delaySeconds = 3;
 var url = "http://api.open-notify.org/iss-now.json";
 
-got(url, { json: true })
-	.then(function(iss) {
-		console.log(iss.body);
-	})
-	.catch(function(error) {
-		console.log(error.response.body);
-	});
+
+function loop(){
+	got(url, { json: true })
+
+		// Get current ISS coordinates
+		.then(function(iss) {
+			var position = iss.body.iss_position;
+			console.log(position);
+		})
+		.catch(function(error) {
+			console.log(error.response.body);
+		});
+
+	// Get ISS position every 3 seconds
+	setTimeout(loop, delaySeconds * 1000);
+}
+
+loop();
