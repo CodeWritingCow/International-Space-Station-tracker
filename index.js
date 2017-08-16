dc5a76bca592b554c991faf75792a7ffd079415d
Gary Pang <gary.pang@gmail.com>
2017-08-16 02:26:54 -0400

    Get distance between station and myPosition

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var got = require('got');
+var got = require('got'); // NPM module for getting Web content with JavaScript promises
+var geolib = require('geolib'); // module for calculating distance, etc.
 
 var delaySeconds = 3;
 var url = "http://api.open-notify.org/iss-now.json";
+var myPosition = { latitude: 40.712784, longitude: -74.005941 }; // Coordinates for New York, NY
 
 
 function loop(){
@@ -10,7 +12,8 @@ function loop(){
 		// Get current ISS coordinates
 		.then(function(iss) {
 			var position = iss.body.iss_position;
-			console.log(position);
+			var distanceFromIss = geolib.getDistance(myPosition, position);
+			console.log(`${distanceFromIss} meters`);
 		})
 		.catch(function(error) {
 			console.log(error.response.body);
