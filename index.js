fb2fbc5991103580e3773388ec90371e7d220c56
Gary Pang <gary.pang@gmail.com>
2017-08-18 17:04:11 -0400

    Log if space station is getting closer or further away

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var geolib = require('geolib'); // module for calculating distance, etc.
 var fs = require('fs');
 var path = require('path');
 var eol = require('os').EOL; // returns end-of-line marker for current OS
+var moment = require('moment'); // module for formatting and parsing dates
 
 var loopSeconds = 10;
 var url = "http://api.open-notify.org/iss-now.json";
@@ -11,6 +12,12 @@ var myPosition = { latitude: 40.712784, longitude: -74.005941 }; // Coordinates 
 var logDir = path.join(__dirname, 'logs');
 var dataLogFile = 'issResults.csv';
 
+var currentDateTime = moment().format('YYMMDD[T]HHmmss');
+var closestDistanceFile = "issClosest-" + currentDateTime + ".csv";
+var furthestDistanceFile = "issFurthest-" + currentDateTime + ".csv";
+
+var closestDistance = Number.MAX_VALUE;
+var furthestDistance = Number.MIN_VALUE;
 
 // check if directory exists
 function directoryExists(filePath) {
@@ -29,12 +36,14 @@ function init() {
 }
 
 // log data with timestamp into csv file
-function logToFile(logFileName, dataToWrite) {
+function logToFile(logFileName, dataToWrite, append = true) {
 	var logFilePath = path.join(logDir, logFileName);
 	var timestamp = new Date().toLocaleString();
 	var data = timestamp + ', ' + dataToWrite + eol;
 
-	fs.appendFile(logFilePath, data, function(error) {
+	var flags = append ? {flag: 'a' } : {};
+
+	fs.writeFile(logFilePath, data, flags, function(error) {
 		if (error) {
 			console.log('Write error to ' + logFileName + ': ' + error.message);
 		}
@@ -53,6 +62,19 @@ function loop(){
 			
 			console.log(`${distanceFromIssMiles} miles`);
 			logToFile(dataLogFile, distanceFromIssMiles);
+
+			if (distanceFromIssMiles < closestDistance) {
+				closestDistance = distanceFromIssMiles;
+				console.log("Closer than ever: " + closestDistance);
+				logToFile(closestDistanceFile, closestDistance, false);
+			}
+
+			if (distanceFromIssMiles > furthestDistance) {
+				furthestDistance = distanceFromIssMiles;
+				console.log("Further than ever: " + furthestDistance);
+				logToFile(furthestDistanceFile, furthestDistance, false);
+			}
+
 		})
 		.catch(function(error) {
 			console.log(error.response.body);
