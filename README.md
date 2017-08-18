2ce1d029ef44a1eddd92f5a0b1a78ed7769a37f7
Gary Pang <gary.pang@gmail.com>
2017-08-18 02:47:22 -0400

    Log distance, timestamp into CSV file

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # International Space Station tracker
 
-This Node.js app tracks the distance between the International Space Station (ISS) and our location on Earth in real time. (Location is currently set to New York, NY.) It runs on the [Open Notify API](http://open-notify.org) by Nathan Bergey that retrieves raw data from NASA.
+This Node.js app tracks the distance between the International Space Station (ISS) and our location on Earth in real time. (Location is currently set to New York, NY.) It logs data into a CSV file. It runs on the [Open Notify API](http://open-notify.org) by Nathan Bergey that retrieves raw data from NASA.
 
-From the tutorial, ["Node.js IoT: Tracking the ISS through the Sky,"](http://thisdavej.com/node-js-iot-tracking-the-iss-through-the-sky) by Dave Johnson.
+From the tutorials ["Node.js IoT: Tracking the ISS through the Sky,"](http://thisdavej.com/node-js-iot-tracking-the-iss-through-the-sky) and ["Node.js IoT: Logging Data That Is Out of This World"](http://thisdavej.com/node-js-iot-logging-data-that-is-out-of-this-world/) by Dave Johnson.
--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 var got = require('got'); // NPM module for getting Web content with JavaScript promises
 var geolib = require('geolib'); // module for calculating distance, etc.
+var fs = require('fs');
+var path = require('path');
+var eol = require('os').EOL; // returns end-of-line marker for current OS
 
-var delaySeconds = 3;
+var loopSeconds = 10;
 var url = "http://api.open-notify.org/iss-now.json";
 var myPosition = { latitude: 40.712784, longitude: -74.005941 }; // Coordinates for New York, NY
+
+var logDir = path.join(__dirname, 'logs');
+var dataLogFile = 'issResults.csv';
+
+
+// check if directory exists
+function directoryExists(filePath) {
+	try {
+		return fs.statSync(filePath).isDirectory();
+	} catch (err) {
+		return false;
+	}
+}
+
+// if log directory doesn't exists, create it
+function init() {
+	if (!directoryExists(logDir)) {
+		fs.mkdirSync(logDir);
+	}
+}
+
+// log data with timestamp into csv file
+function logToFile(logFileName, dataToWrite) {
+	var logFilePath = path.join(logDir, logFileName);
+	var timestamp = new Date().toLocaleString();
+	var data = timestamp + ', ' + dataToWrite + eol;
+
+	fs.appendFile(logFilePath, data, function(error) {
+		if (error) {
+			console.log('Write error to ' + logFileName + ': ' + error.message);
+		}
+	});
+}
 
 
 function loop(){
@@ -14,14 +50,17 @@ function loop(){
 			var position = iss.body.iss_position;
 			var distanceFromIss = geolib.getDistance(myPosition, position);
 			var distanceFromIssMiles = geolib.convertUnit('mi', distanceFromIss, 2);
+			
 			console.log(`${distanceFromIssMiles} miles`);
+			logToFile(dataLogFile, distanceFromIssMiles);
 		})
 		.catch(function(error) {
 			console.log(error.response.body);
 		});
 
 	// Get ISS position every 3 seconds
-	setTimeout(loop, delaySeconds * 1000);
+	setTimeout(loop, loopSeconds * 1000);
 }
 
+init();
 loop();
